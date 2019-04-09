0294978a318d34d8a6c045bbce0e9305105488ec
Daniel Espitia <36012869+daespitiab@users.noreply.github.com>
2019-04-09 18:00:04 -0500

    Create persona.service.ts

---
+++ src/app/persona/persona.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http';
+import{Persona} from './persona';
+import{PersonaDetail} from './persona-detail';
+import{Observable} from 'rxjs';
+
+const API_URL="../../assets/";
+const clientes='personas.json'
+@Injectable()
+export class PersonaService {
+
+  constructor(private http: HttpClient) { }
+  
+  getPersonasDetail():Observable<PersonaDetail[]>{
+    return this.http.get<PersonaDetail[]>(API_URL+clientes);
+  }
+  getPersonaDetail(personaId:number): Observable<PersonaDetail> {
+        return this.http.get<PersonaDetail>(API_URL + "persona" + personaId+".json");
+    }
+}
