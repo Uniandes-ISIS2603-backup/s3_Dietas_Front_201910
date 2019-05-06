5ee936d6ae4028ebf951804730660405769f39b8
Juan Antonio Restrepo <juan981106@hotmail.com>
2019-05-06 08:42:19 -0500

    Poner bien mela la interfaz
    Bien mela bien mela, una chimba, heeheeeeeeee ayuwoki

--- src/app/tipo-dieta/tipo-dieta.module.ts
+++ src/app/tipo-dieta/tipo-dieta.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TipoDietaListarComponent } from './tipo-dieta-listar/tipo-dieta-listar.component';
+import { TipoDietaService } from './tipo-dieta.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [TipoDietaListarComponent],
-  exports:[TipoDietaListarComponent]
+  exports:[TipoDietaListarComponent],
+  providers:[TipoDietaService]
 })
 export class TipoDietaModule { }
